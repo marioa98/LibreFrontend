e0a043fecebc960169718696fbd89e77f44bb898
gerrylarios <gchavez8@ucol.mx>
2018-11-20 22:22:26 -0600

    [ADD] the home user

---
+++ src/components/Archivos.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="card">
+    <div class="card-image">
+      <figure class="image is-1by1">
+        <img src="../assets/logo.png">
+      </figure>
+    </div>
+    <div class="card-content">
+      <div class="media">
+        <div class="media-left">
+          <figure class="image is-1by1">
+            <img src="../assets/logo.png">
+          </figure>
+        </div>
+        <div class="media-content">
+            <p class="title is-6">
+                <strong>
+                    {{ archivo.nombre }}
+                </strong>
+            </p>
+            <p class="title is-6">
+                {{ archivo.autores[0].nombre }}
+            </p>
+        </div>
+        <div class="content">
+            <small>
+                {{ archivo.descripcion}}
+            </small>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+  export default {
+      name: 'Archivos',
+      props: {
+          archivo: {
+              type: Object,
+              required: true
+          }
+      }
+  }
+</script>
---
+++ src/pages/UserHome.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <user-navbar />
+    <user-searchbar />
+
+    <div class="container results">
+      <div class="columns is-multiline">
+        <div class="column is-one-quarter">
+          <libre-archivo v-for="archivo in archivos" v-bind:archivo="archivo" v-bind:key="archivo.id" />
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+<script>
+  import UserNavbar from '../components/layout/UserNavbar.vue'
+  import UserSearchbar from '../components/layout/UserSearchbar.vue'
+  import LibreArchivo from '../components/Archivos.vue'
+  export default {
+    name: 'UserHome',
+    components: {
+      UserNavbar,
+      UserSearchbar,
+      LibreArchivo
+    },
+    data() {
+      return {
+        archivos: [{
+            "id": 2,
+            "nombre": "Matadero Cinco",
+            "descripcion": "Descripción no disponible",
+            "uri": "https://www.gandhi.com.mx/matadero-cinco",
+            "img": "fuck base64",
+            "activo": true,
+            "createdAt": "2018-11-19T19:10:05.368+0000",
+            "updatedAt": "2018-11-19T19:10:05.368+0000",
+            "autores": [{
+              "id": 1,
+              "nombre": "Charles Bukowski",
+              "alias": "Bukowski",
+              "createdAt": "2018-11-19T19:13:08.177+0000",
+              "updatedAt": "2018-11-19T19:13:08.177+0000"
+            }],
+            "etiquetas": [],
+            "comentarios": [],
+            "tipos": null
+          },
+          {
+            "id": 2,
+            "nombre": "Matadero Cinco",
+            "descripcion": "Descripción no disponible",
+            "uri": "https://www.gandhi.com.mx/matadero-cinco",
+            "img": "fuck base64",
+            "activo": true,
+            "createdAt": "2018-11-19T19:10:05.368+0000",
+            "updatedAt": "2018-11-19T19:10:05.368+0000",
+            "autores": [{
+              "id": 1,
+              "nombre": "Charles Bukowski",
+              "alias": "Bukowski",
+              "createdAt": "2018-11-19T19:13:08.177+0000",
+              "updatedAt": "2018-11-19T19:13:08.177+0000"
+            }],
+            "etiquetas": [],
+            "comentarios": [],
+            "tipos": null
+          }
+        ],
+      }
+    }
+  }
+
+</script>
+<style>
+  .results {
+    margin-top: 50px;
+  }
+
+</style>
