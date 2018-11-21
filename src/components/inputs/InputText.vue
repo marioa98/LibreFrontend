113fa7586dc433e3ad7538cfe2d715df53885403
gerrylarios <gchavez8@ucol.mx>
2018-11-20 20:28:11 -0600

    [ADD] SingUp

--- src/components/inputs/InputText.vue
+++ src/components/inputs/InputText.vue
@@ -2,7 +2,7 @@
   <div class="field">
     <label class="label" for="fieldId">Some text</label>
     <div class="control">
-        <input class="input" type="text" name="somename" id="fieldId" placeholder="text input">
+      <input class="input" type="text" name="somename" id="fieldId" placeholder="text input">
     </div>
   </div>
 </template>
