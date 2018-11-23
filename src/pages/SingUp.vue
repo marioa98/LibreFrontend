a00b7d8933cb6378ce53b452a55ae5016e190a2c
gerrylarios <gchavez8@ucol.mx>
2018-11-23 12:06:45 -0600

    [REF] inputs texts

--- src/pages/SingUp.vue
+++ src/pages/SingUp.vue
@@ -9,15 +9,16 @@
             <div class="box">
               <form>
                 <!-- nombre -->
-                <input-text />
+                <input-text v-bind:field="fields[0]" />
                 <!-- apellidos -->
-                <input-text />
+                <input-text v-bind:field="fields[1]" />
                 <!-- correo -->
-                <input-text />
+                <input-text v-bind:field="fields[2]" />
                 <!-- password -->
-                <input-text />
+                <input-text v-bind:field="fields[3]" />
                 <!-- facultad -->
-                <input-text />
+                <input-text v-bind:field="fields[4]" />
+                
                 <button class="button is-medium is-fullwidth">Crear una cuenta</button>
               </form>
             </div>
@@ -33,6 +34,56 @@
     name: 'Login',
     components: {
       InputText
+    },
+    data() {
+      return {
+        fields: [{
+            id: 1,
+            ident: 'UsuarioName',
+            label: 'Nombre',
+            name: 'name',
+            placeholder: '',
+            value: '',
+            type: 'text'
+          },
+          {
+            id: 2,
+            ident: 'UsuarioLastName',
+            label: 'Apellidos',
+            name: 'lastname',
+            placeholder: '',
+            value: '',
+            type: 'text'
+          },
+          {
+            id: 3,
+            ident: 'UsuarioEmail',
+            label: 'Email',
+            name: 'email',
+            placeholder: '',
+            value: '',
+            type: 'email'
+          },
+          {
+            id: 4,
+            ident: 'UsuarioPassword',
+            label: 'Password',
+            name: 'password',
+            placeholder: '',
+            value: '',
+            type: 'password'
+          },
+          {
+            id: 5,
+            ident: 'UsuarioFacultad',
+            label: 'Facultad',
+            name: 'facultad',
+            placeholder: '',
+            value: '',
+            type: 'text'
+          }
+        ]
+      }
     }
   }
 
