8cb6148b1bed203d2e050dbe1bcfc6303aad59b4
gerrylarios <gchavez8@ucol.mx>
2018-11-23 20:23:51 -0600

    [ADD] admin services

--- src/services/administrador.js
+++ src/services/administrador.js
@@ -1,34 +1,30 @@
 import libreBackendService from './libre-backend'
 
-const requestMapping = '/usuarios'
-const usuarioService = {}
+const requestMapping = '/administradores'
+const administradorService = {}
 
-usuarioService.agregar = function (body) {
+administradorService.agregar = function (body) {
   return libreBackendService.post(requestMapping, body).then((res) => res.data)
 }
 
-usuarioService.obtenerTodos = function () {
+administradorService.obtenerTodos = function () {
   return libreBackendService.get(requestMapping).then((res) => res.data)
 }
 
-usuarioService.obtenerPorId = function (id) {
+administradorService.obtenerPorId = function (id) {
   return libreBackendService.get(`${requestMapping}/${id}`).then((res) => res.data)
 }
 
-usuarioService.modificar = function (id, body) {
+administradorService.modificar = function (id, body) {
   return libreBackendService.put(`${requestMapping}/${id}`, body).then((res) => res.data)
 }
 
-usuarioService.eliminar = function (id) {
+administradorService.eliminar = function (id) {
   return libreBackendService.delete(`${requestMapping}/${id}`).then((res) => res.data)
 }
 
-usuarioService.agregarFacultad = function (id, body) {
-    return libreBackendService.put(`${requestMapping}/${id}`, body).then((res) => res.data)
-}
-
-usuarioService.login = function (body) {
+administradorService.login = function (body) {
   return libreBackendService.post(`${requestMapping}/login`, body).then((res) => res.data)
 }
 
-export default usuarioService
+export default administradorService
