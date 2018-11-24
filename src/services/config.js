d4f7243b37047ff2abad2d92c4caf700e21ce635
gerrylarios <gchavez8@ucol.mx>
2018-11-23 20:20:02 -0600

    [ADD] usuarios services

---
+++ src/services/config.js
@@ -0,0 +1,5 @@
+const config = {
+  apiUrl: 'https://librebackend.herokuapp.com'
+}
+
+export default config
---
+++ src/services/usuario.js
@@ -0,0 +1,32 @@
+import libreBackendService from './libre-backend'
+
+const requestMapping = '/usuarios'
+const usuarioService = {}
+
+usuarioService.agregar = function (body) {
+  return libreBackendService.post(requestMapping, body).then((res) => res.data)
+}
+
+usuarioService.obtenerTodos = function () {
+  return libreBackendService.get(requestMapping).then((res) => res.data)
+}
+
+usuarioService.obtenerPorId = function (id) {
+  return libreBackendService.get(`${requestMapping}/${id}`).then((res) => res.data)
+}
+
+usuarioService.modificar = function (id, body) {
+  return libreBackendService.put(`${requestMapping}/${id}`, body).then((res) => res.data)
+}
+
+usuarioService.eliminar = function (id) {
+  return libreBackendService.delete(`${requestMapping}/${id}`).then((res) => res.data)
+}
+
+usuarioService.agregarFacultad = function (id, body) {
+    return libreBackendService.put(`${requestMapping}/${id}`, body).then((res) => res.data)
+}
+
+usuarioService.login = function (body) {
+  return libreBackendService.post(`${requestMapping}/login`, body).then((res) => res.data)
+}
