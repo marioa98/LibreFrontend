dc7213b8fd4700da968ba6c8b791c16128690327
gerrylarios <gchavez8@ucol.mx>
2018-11-26 06:46:53 -0600

    [REF] sing up inputs

--- src/pages/SingUp.vue
+++ src/pages/SingUp.vue
@@ -7,18 +7,45 @@
             <h3 class="title has-text-centered">Unirse a LIBRE</h3>
             <p class="subtitle has-text-centered">Cree una cuenta personal</p>
             <div class="box">
-              <form>
-                <!-- nombre -->
-                <input-text v-bind:field="fields[0]" />
-                <!-- apellidos -->
-                <input-text v-bind:field="fields[1]" />
-                <!-- correo -->
-                <input-text v-bind:field="fields[2]" />
-                <!-- password -->
-                <input-text v-bind:field="fields[3]" />
-                <!-- facultad -->
-                <input-text v-bind:field="fields[4]" />
-
+              <form v-on:submit.prevent="submit">
+                <div class="field">
+                  <label for="UsuarioName">Nombre</label>
+                  <div class="control">
+                    <input class="input" type="text" name="name" id="UsuarioName" placeholder="Ingrese su nombre"
+                      v-model="nombre">
+                  </div>
+                </div>
+                <div class="field">
+                  <label for="UsuarioLastName">Apellidos</label>
+                  <div class="control">
+                    <input class="input" type="text" name="lastname" id="UsuarioLastName" placeholder="Ingrese su apellido"
+                      v-model="apellidos">
+                  </div>
+                </div>
+                <div class="field">
+                  <label for="UsuarioEmail">Email</label>
+                  <div class="control">
+                    <input class="input" type="email" name="email" id="UsuarioEmail" placeholder="e.j. alexsmith@gmail.com"
+                      v-model="correo">
+                  </div>
+                </div>
+                <div class="field">
+                  <label for="UsuarioPassword">Password</label>
+                  <div class="control">
+                    <input class="input" type="password" name="password" id="UsuarioPassword" v-model="password">
+                  </div>
+                </div>
+                <div class="field">
+                  <label class="label">Facultad</label>
+                  <div class="control">
+                    <div class="select">
+                      <select v-model="facultad">
+                        <option value=""></option>
+                      </select>
+                    </div>
+                  </div>
+                </div>
+                <p> {{ facultad }} </p>
                 <button class="button is-medium is-fullwidth">Crear una cuenta</button>
               </form>
             </div>
@@ -30,60 +57,23 @@
 </template>
 <script>
   import InputText from '../components/inputs/InputText.vue';
+  import facultadService from '../services/facultad'
+
   export default {
     name: 'Login',
-    components: {
-      InputText
-    },
     data() {
       return {
-        fields: [{
-            id: 1,
-            ident: 'UsuarioName',
-            label: 'Nombre',
-            name: 'name',
-            placeholder: '',
-            value: '',
-            type: 'text'
-          },
-          {
-            id: 2,
-            ident: 'UsuarioLastName',
-            label: 'Apellidos',
-            name: 'lastname',
-            placeholder: '',
-            value: '',
-            type: 'text'
-          },
-          {
-            id: 3,
-            ident: 'UsuarioEmail',
-            label: 'Email',
-            name: 'email',
-            placeholder: '',
-            value: '',
-            type: 'email'
-          },
-          {
-            id: 4,
-            ident: 'UsuarioPassword',
-            label: 'Password',
-            name: 'password',
-            placeholder: '',
-            value: '',
-            type: 'password'
-          },
-          {
-            id: 5,
-            ident: 'UsuarioFacultad',
-            label: 'Facultad',
-            name: 'facultad',
-            placeholder: '',
-            value: '',
-            type: 'text'
-          }
-        ]
+        nombre: '',
+        apellidos: '',
+        correo: '',
+        activo: true,
+        password: '',
+        facultad: '',
+        facultades: []
       }
+    },
+    mounted () {
+
     }
   }
 
